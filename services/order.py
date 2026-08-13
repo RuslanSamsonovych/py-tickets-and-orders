6ae812b2e72d8292ea0c085e768917f0b1d09ae8
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2026-08-13 16:21:12 +0300

    implement Tickets and orders

--- services/order.py
+++ services/order.py
@@ -25,7 +25,7 @@ def create_order(
         )
 
 
-def get_orders(username: str | None = None) -> QuerySet:
+def get_orders(username: str | None = None) -> QuerySet[Order]:
     orders = Order.objects.all()
     if username:
         orders = orders.filter(user__username=username)
--- services/user.py
+++ services/user.py
@@ -7,7 +7,7 @@ User = get_user_model()
 def create_user(
     username: str,
     password: str,
-    email: str = "",
+    email: str | None = None,
     first_name: str = "",
     last_name: str = ""
 ) -> User:
@@ -26,23 +26,23 @@ def get_user(user_id: int) -> User:
 
 def update_user(
     user_id: int,
-    username: str = "",
-    password: str = "",
-    email: str = "",
-    first_name: str = "",
-    last_name: str = ""
+    username: str | None = None,
+    password: str | None = None,
+    email: str | None = None,
+    first_name: str | None = None,
+    last_name: str | None = None
 ) -> User:
     user = get_user(user_id)
 
-    if username:
+    if username is not None:
         user.username = username
-    if password:
+    if password is not None:
         user.set_password(password)
-    if email:
+    if email is not None:
         user.email = email
-    if first_name:
+    if first_name is not None:
         user.first_name = first_name
-    if last_name:
+    if last_name is not None:
         user.last_name = last_name
 
     user.save()
